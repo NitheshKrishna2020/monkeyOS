a32ed1e227781279d337b286210a5ae3286801f2
hounangogogo <1294030303@qq.com>
2021-08-31 17:41:41 -0400

    added message

--- boot.asm
+++ boot.asm
@@ -1,12 +1,29 @@
-org 0
+org 0x7c00  ; offset 0x7c00
 bits 16
 
 start:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0
-    int 0x10
+    mov bx, message     
+    call print_message
     jmp $
+
+print_message:
+    mov al, [bx]
+    cmp al, 0
+    je .done
+    call print_char
+    inc bx
+    jmp print_message
+
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message:
+    db 'Hello From My Kernel!', 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
