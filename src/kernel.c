393da0c2823e4dfc2c1dd4da986ea969229cb4ea
hounangogogo <1294030303@qq.com>
2021-09-02 12:34:04 -0400

    added print string

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,32 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
 // init the video memory pointer
 uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
+
+
+// terminal make a char (16 bits)
 uint16_t terminal_make_char(char c, char color) {
     return (color << 8 | c);
+}
+
+// put a char on screen
+void terminal_putchar(int x, int y, char c, char color) {
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+// write a char to screen, and adjust current position
+void terminal_writechar(char c, char color) {
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+    if(terminal_col >= VGA_WIDTH) {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
 }
 
 
@@ -22,9 +43,24 @@ void terminal_initialize() {
 }
 
 
+// calculate the len of a string
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while(str[len]) {
+        len++;
+    }
+    return len;
+}
+
+void print(const char* str) {
+    size_t len = strlen(str);
+    for(int i = 0; i < len; i++) {
+        terminal_writechar(str[i], 15);
+    }
+}
+
 
 void kernel_main() {
     terminal_initialize();
-
-    video_mem[0] = 0x0241;
+    print("Hello World");
 }
